71dc9308e045692649ab9599eb06c2a6b78cc453
b-arslan <bugraarslan5@outlook.com>
2024-09-08 15:34:52 +0300

    profile picture removed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -30,9 +30,9 @@ const About = () => {
 
             <Content className={styles.content}>
                 <Row style={{ height: '100%' }}>
-                    <Col span={12} style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    {/* <Col span={12} style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                         <Image src={ProfileImg} alt='profile image' className={styles.myImg} />
-                    </Col>
+                    </Col> */}
 
                     <Col span={12} style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
                         <div className={styles.myImg} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', position: 'relative' }}>
