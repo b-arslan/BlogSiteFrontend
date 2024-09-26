1bd01627562d5d495adde9acf4d5b24d5637e8d0
Bugra Arslan <bugraarslan5@outlook.com>
2024-09-26 23:39:32 +0300

    .env updated

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ import { env } from 'process';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: process.env.API_URL
+    API_URL: process.env.API_URL,
+    EMAILJS_PUBLIC_KEY: process.env.EMAILJS_PUBLIC_KEY,
+    EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+    EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID
   },
   experimental: {
     missingSuspenseWithCSRBailout: false,
