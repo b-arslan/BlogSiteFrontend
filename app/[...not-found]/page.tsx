cc46eb704604afe2856514015ab06154a2991451
Bugra Arslan <bugraarslan5@outlook.com>
2024-10-29 15:33:19 +0300

    not found page updated

--- app/[...not-found]/page.tsx
+++ app/[...not-found]/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 import { LoadingOutlined } from '@ant-design/icons';
-import { Spin } from 'antd';
+import { Button, Spin } from 'antd';
 import { useEffect, useState } from 'react';
+import styles from '../styles/page.module.scss';
 
 
 const NotFoundPage = () => {
@@ -9,22 +10,22 @@ const NotFoundPage = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        // Hide header and footer on admin page
+        // Hide header and footer on not found page
         const header = document.getElementById('header-component');
-        const footer = document.getElementById('footer-component');
-
-        if (header) header.style.display = 'none';
-        if (footer) footer.style.display = 'none';
+        debugger
+        setTimeout(() => {
+            const footer = document.getElementById('footer-component');
+            if (footer) footer.style.display = 'none';
+            if (header) header.style.display = 'none';
+            // return () => {
+            //     if (footer) footer.style.display = 'block';
+            //     if (header) header.style.display = 'block';
+            // };
+        }, 50);
 
         setTimeout(() => {
             setLoading(false);
         }, 500);
-
-        // Clean up when component unmounts
-        return () => {
-            if (header) header.style.display = 'block';
-            if (footer) footer.style.display = 'block';
-        };
     }, []);
 
     if (loading) {
@@ -37,8 +38,9 @@ const NotFoundPage = () => {
 
     return (
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column', gap: '2rem'}}>
-            <h1 style={{textAlign: 'center'}}>404 - Sayfa Bulunamadı</h1>
-            <p>Üzgünüz, aradığınız sayfa mevcut değil.</p>
+            <h1 style={{textAlign: 'center', fontSize: '48px', fontWeight: '800'}}>Sayfa Bulunamadı</h1>
+            <p style={{fontSize: '18px'}}>Üzgünüz, aradığınız sayfa mevcut değil.</p>
+            <Button type='primary' className={styles.notFoundBtn} href='/'>Ana Sayfaya Dön</Button>
         </div>
     );
 };
