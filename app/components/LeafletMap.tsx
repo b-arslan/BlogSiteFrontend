a9666a4b00fca851ad495755d37f7636733d5543
Bugra Arslan <bugraarslan5@outlook.com>
2024-10-31 20:44:53 +0300

    google maps direction added to leaflet

--- app/components/LeafletMap.tsx
+++ app/components/LeafletMap.tsx
@@ -1,41 +1,60 @@
-'use client';
-import { useEffect, useRef } from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import styles from '../styles/page.module.scss';
+"use client";
+import { useEffect, useRef } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import styles from "../styles/page.module.scss";
 
 const LeafletMap = () => {
     const mapContainerRef = useRef<HTMLDivElement>(null);
     const mapInstanceRef = useRef<L.Map | null>(null);
 
     useEffect(() => {
+        const coordinates: L.LatLngExpression = [
+            37.86994421452708, 32.474147158457434,
+        ]; // Coordinates for Konya I&G Danışmanlık
 
-        const coordinates: L.LatLngExpression = [37.86994421452708, 32.474147158457434]; // Coordinates for Konya I&G Danışmanlık
-    
         if (!mapInstanceRef.current && mapContainerRef.current) {
-          const map = L.map(mapContainerRef.current).setView(coordinates, 13); // Center map on Konya I&G Danışmanlık
-    
-          // Use Stadia Maps tiles for a more modern look
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-          }).addTo(map);  
-    
-          // Custom marker icon resembling Google Maps pin
-          const customIcon = L.icon({
-            iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/1200px-Map_marker.svg.png', // Apple Maps-like pin,
-            iconSize: [25, 40], // Size of the icon
-            iconAnchor: [13, 40], // Point of the icon which will correspond to marker's location
-            popupAnchor: [0, -40] // Point from which the popup should open relative to the iconAnchor
-          });
-    
-          // Add a custom marker at Konya I&G Danışmanlık location
-          L.marker(coordinates, { icon: customIcon }).addTo(map)
-            .bindPopup('İ&G Danışmanlık', { closeButton: false }) // Customize the popup text
-            .openPopup(); // Popup will be open by default
-    
-          mapInstanceRef.current = map; // Store the map instance in the ref
+            const map = L.map(mapContainerRef.current).setView(coordinates, 13); // Center map on Konya I&G Danışmanlık
+
+            // Use Stadia Maps tiles for a more modern look
+            L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                attribution:
+                    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            }).addTo(map);
+
+            // Custom marker icon resembling Google Maps pin
+            const customIcon = L.icon({
+                iconUrl:
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Map_marker.svg/1200px-Map_marker.svg.png", // Apple Maps-like pin,
+                iconSize: [25, 40], // Size of the icon
+                iconAnchor: [13, 40], // Point of the icon which will correspond to marker's location
+                popupAnchor: [0, -40], // Point from which the popup should open relative to the iconAnchor
+            });
+
+            const googleMapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${coordinates[0]},${coordinates[1]}`;
+
+            const popupContent = document.createElement("div");
+            popupContent.innerHTML = "İ&G Danışmanlık";
+            popupContent.style.cursor = "pointer";
+            popupContent.addEventListener("click", () => {
+                window.open(googleMapsUrl, "_blank");
+            });
+
+            const popup = L.popup({ closeButton: false, closeOnClick: false })
+                .setLatLng(coordinates)
+                .setContent(popupContent);
+
+            const marker = L.marker(coordinates, { icon: customIcon })
+                .addTo(map)
+                .on("click", () => {
+                    window.open(googleMapsUrl, "_blank"); 
+                })
+                .bindPopup(popup)
+                .openPopup();
+
+            mapInstanceRef.current = map; // Store the map instance in the ref
         }
-    
+
         return () => {
             if (mapInstanceRef.current) {
                 mapInstanceRef.current.remove();
